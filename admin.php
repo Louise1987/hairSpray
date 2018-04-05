ad3cf07ccb17a3dd40baba208eb13542105be0ca
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2018-04-05 13:17:45 +0200

    admin

---
+++ admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+
+<div id="inlogg">
+<form>
+Namn: <input type="text" name="user">
+Lösenord: <input type="password" name="pass">
+<input type="submit" name="submit" value="Logga in">
+</form>
+</div>
+
+<?php
+
+
+
+?>
+    
+</body>
+</html>
