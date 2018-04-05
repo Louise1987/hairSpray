45eb593854e05218fae62a62bd73918ba03500f2
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2018-04-05 14:34:34 +0200

    admin+db

--- admin.php
+++ admin.php
@@ -1,3 +1,23 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$database="demo";
+
+mysql_connect($host,$user,$password);
+$mysql_select_database($database);
+
+if(isset(['user'])){
+$uname=$_POST['user'];
+$password=$_POST['pass'];
+
+$sql="select * from Admin where username='".$uname."'AND password"
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,18 +29,14 @@
 <body>
 
 <div id="inlogg">
-<form>
-Namn: <input type="text" name="user">
-Lösenord: <input type="password" name="pass">
+<form method="POST" action="#">
+ <input type="text" name="user" placeholder="Namn">
+ <input type="password" name="pass" placeholder="Lösenord">
 <input type="submit" name="submit" value="Logga in">
 </form>
 </div>
 
-<?php
 
-
-
-?>
     
 </body>
 </html>
